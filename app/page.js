415e25925eb0dc7ee9f0cf36fc45bb3986e9058e
Avni17 <avni.arora2002@gmail.com>
2023-11-15 18:42:04 +0530

    landing page created

--- app/page.js
+++ app/page.js
@@ -1,20 +1,42 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react';
+import AccountCreation from './register/page';
+import UserDetails from './user-details/page';
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState('userDetails');
+
   return (
-    <>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/user-details">User Details</Link>
-        </li>
-        <li>
-          <Link href="/register">Account Creation</Link>
-        </li>
-      </ul>
-    </>
-  )
+    <div className="min-h-screen">
+      <header className="py-4">
+        <nav className="flex justify-center">
+          <button
+            onClick={() => setActiveTab('userDetails')}
+            className={`px-6 py-2 focus:outline-none ${
+              activeTab === 'userDetails'
+                ? 'bg-blue-500 text-white'
+                : 'text-blue-500 hover:bg-blue-200'
+            }`}
+          >
+            User Details
+          </button>
+          <button
+            onClick={() => setActiveTab('accountCreation')}
+            className={`px-6 py-2 focus:outline-none ${
+              activeTab === 'accountCreation'
+                ? 'bg-blue-500 text-white'
+                : 'text-blue-500 hover:bg-blue-200'
+            }`}
+          >
+            Account Creation
+          </button>
+        </nav>
+      </header>
+      <main className="p-4">
+        {activeTab === 'userDetails' ? <UserDetails /> : <AccountCreation />}
+      </main>
+    </div>
+  );
 }
