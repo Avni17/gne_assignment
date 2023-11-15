52eb5fba3075c2f0802496954765fcd46ff7119a
Avni17 <avni.arora2002@gmail.com>
2023-11-15 19:04:09 +0530

    account creation modified

--- app/register/page.js
+++ app/register/page.js
@@ -50,7 +50,7 @@ const Register = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Enter your password"
-                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-black"
                 required
               />
             </div>
