02970f4bf2e3a7f190e844a7acadbc2eaf5051fa
Avni17 <avni.arora2002@gmail.com>
2023-11-15 19:47:07 +0530

    tab animation changed

--- app/page.js
+++ app/page.js
@@ -8,29 +8,44 @@ import UserDetails from './user-details/page';
 export default function Home() {
   const [activeTab, setActiveTab] = useState('userDetails');
 
+  const tabUnderlineStyle = {
+    width: activeTab === 'userDetails' ? '50%' : '50%',
+    transform: activeTab === 'userDetails' ? 'translateX(0)' : 'translateX(100%)',
+  };
+
   return (
     <div className="min-h-screen">
       <header className="py-4">
         <nav className="flex justify-center">
           <button
             onClick={() => setActiveTab('userDetails')}
-            className={`px-6 py-2 focus:outline-none ${
-              activeTab === 'userDetails'
-                ? 'bg-blue-500 text-white'
-                : 'text-blue-500 hover:bg-blue-200'
-            }`}
+            className={`relative px-6 py-2 focus:outline-none ${activeTab === 'userDetails'
+                ? 'text-blue-500'
+                : 'text-gray-500 hover:text-blue-500'
+              }`}
           >
             User Details
+            {activeTab === 'userDetails' && (
+              <div
+                className="absolute bottom-0 left-0 w-full h-0.5 bg-blue-500"
+                style={tabUnderlineStyle}
+              ></div>
+            )}
           </button>
           <button
             onClick={() => setActiveTab('accountCreation')}
-            className={`px-6 py-2 focus:outline-none ${
-              activeTab === 'accountCreation'
-                ? 'bg-blue-500 text-white'
-                : 'text-blue-500 hover:bg-blue-200'
-            }`}
+            className={`relative px-6 py-2 focus:outline-none ${activeTab === 'accountCreation'
+                ? 'text-blue-500'
+                : 'text-gray-500 hover:text-blue-500'
+              }`}
           >
             Account Creation
+            {activeTab === 'accountCreation' && (
+              <div
+                className="absolute bottom-0 left-0 w-full h-0.5 bg-blue-500"
+                style={tabUnderlineStyle}
+              ></div>
+            )}
           </button>
         </nav>
       </header>
