20536b6a20d6aae5c2b100af1e5cadf08c657823
Avni17 <avni.arora2002@gmail.com>
2023-11-15 17:41:48 +0530

    close button fixed

--- app/user-details/modal.js
+++ app/user-details/modal.js
@@ -9,7 +9,7 @@ const Modal = ({ isOpen, onClose, children }) => {
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
             <div className="bg-white p-8 rounded-lg shadow-lg text-black w-96 h-80">
                 <div className="modal-content">
-                    <button style={{right:'-17 rem'}}className="modal-close-btn mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 relative z-50" onClick={onClose}>
+                    <button className="modal-close-btn mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 relative right-[-17rem] z-50" onClick={onClose}>
                         Close
                     </button>
                     {children}
