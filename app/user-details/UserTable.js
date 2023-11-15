154dbb737b2d0ab737e0efc5960ffdb8541bca86
Avni17 <avni.arora2002@gmail.com>
2023-11-15 15:09:57 +0530

    table added

--- app/user-details/UserTable.js
+++ app/user-details/UserTable.js
@@ -30,28 +30,31 @@ const UserDetails = () => {
                 onChange={handleSearch}
             />
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Creation Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredUsers.map((user) => (
-                        <tr key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.username}</td>
-                            <td>{user.email}</td>
-                            <td>{user.phone}</td>
-                            <td>{user.creationDate}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+<div className="overflow-x-auto">
+  <table className="table-auto w-full border-collapse border border-gray-300">
+    <thead className="bg-black-200">
+      <tr>
+        <th className="border border-gray-300 px-4 py-2">ID</th>
+        <th className="border border-gray-300 px-4 py-2">Username</th>
+        <th className="border border-gray-300 px-4 py-2">Email</th>
+        <th className="border border-gray-300 px-4 py-2">Phone</th>
+        <th className="border border-gray-300 px-4 py-2">Creation Date</th>
+      </tr>
+    </thead>
+    <tbody>
+      {filteredUsers.map((user) => (
+        <tr key={user.id} className="hover:bg-gray-100">
+          <td className="border border-gray-300 px-4 py-2">{user.id}</td>
+          <td className="border border-gray-300 px-4 py-2">{user.username}</td>
+          <td className="border border-gray-300 px-4 py-2">{user.email}</td>
+          <td className="border border-gray-300 px-4 py-2">{user.phone}</td>
+          <td className="border border-gray-300 px-4 py-2">{user.creationDate}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
         </div>
     );
 };
