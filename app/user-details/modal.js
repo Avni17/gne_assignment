7202bbf6b14e1cdd4a7bfce82fcd07996709beb1
Avni17 <avni.arora2002@gmail.com>
2023-11-16 13:58:14 +0530

    report

--- app/user-details/modal.js
+++ app/user-details/modal.js
@@ -7,11 +7,15 @@ const Modal = ({ isOpen, onClose, children }) => {
 
     return (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
+            <head>        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+            </head>
             <div className="bg-white p-8 rounded-lg shadow-lg text-black w-96 h-80">
                 <div className="modal-content">
-                    <button className="modal-close-btn mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 relative right-[-17rem] z-50" onClick={onClose}>
-                        Close
-                    </button>
+                    <i
+                        className="modal-close-btn bg-rose-500 cursor-pointer relative right-[-18rem] top-[-0.5rem] z-50 fa fa-window-close"
+                        style={{ color: 'red', fontSize: '2em' }}
+                        onClick={onClose}
+                    ></i>
                     {children}
 
                 </div>
--- app/user-details/report.js
+++ app/user-details/report.js
@@ -8,68 +8,65 @@ const Report = ({ selectedRow }) => {
         const pdfDoc = await PDFDocument.create();
         const page = pdfDoc.addPage();
 
-        page.drawText(`ID: ${selectedRow.id}`, {
-            x: 50,
-            y: 700,
-            size: 30,
-            color: rgb(0, 0, 0),
-        });
-        page.drawText(`Name: ${selectedRow.username}`, {
-            x: 50,
-            y: 650,
-            size: 20,
-            color: rgb(0, 0, 0),
-        });
-        page.drawText(`Email: ${selectedRow.email}`, {
-            x: 50,
-            y: 600,
-            size: 20,
-            color: rgb(0, 0, 0),
-        });
-        page.drawText(`Email: ${selectedRow.email}`, {
-            x: 50,
-            y: 600,
-            size: 20,
-            color: rgb(0, 0, 0),
-        });
-        page.drawText(`Phone: ${selectedRow.phone}`, {
-            x: 50,
-            y: 550,
-            size: 20,
-            color: rgb(0, 0, 0),
-        });
-        page.drawText(`Creation Date: ${selectedRow.creationDate}`, {
-            x: 50,
-            y: 500,
-            size: 20,
-            color: rgb(0, 0, 0),
-        });
-        // Add other details as needed
+        //  const font = await pdfDoc.embedFont(PDFDocument.Font.Helvetica);
 
+        const drawText = (text, x, y, size) => {
+            page.drawText(text, {
+                x,
+                y,
+
+                size,
+                color: rgb(0, 0, 0),
+            });
+        };
+
+        drawText(`User Analysis Report`, 150, 800, 30);
+        drawText(`ID: ${selectedRow.id}`, 50, 700, 20);
+        drawText(`Name: ${selectedRow.username}`, 50, 650, 20);
+        drawText(`Email: ${selectedRow.email}`, 50, 600, 20);
+        drawText(`Phone: ${selectedRow.phone}`, 50, 550, 20);
+        drawText(`Creation Date: ${selectedRow.creationDate}`, 50, 500, 20);
+
+        // Get the bytes of the PDF
         const pdfBytes = await pdfDoc.save();
-        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-        const url = URL.createObjectURL(blob);
 
-        // Open a new window with the PDF for printing
-        const printWindow = window.open(url, '_blank');
-        if (printWindow) {
-            printWindow.print();
-        } else {
-            console.error('Could not open print window.');
-        }
+        // Convert the bytes to a Blob
+        const pdfBlob = new Blob([pdfBytes], { type: 'application/pdf' });
+
+        // Create a URL for the Blob
+        const pdfUrl = URL.createObjectURL(pdfBlob);
+
+        // Open the PDF in a new tab
+        window.open(pdfUrl, '_blank');
     };
 
     return (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-40">
+
+
             <div className="bg-white p-6 rounded-lg shadow-lg text-black w-96 h-80">
-                <h2 className="text-2xl font-bold mb-4">Report</h2>
-                <h3>Id: {selectedRow.id}</h3>
-                <h3>Name: {selectedRow.username}</h3>
-                <h3>Email: {selectedRow.email}</h3>
-                <h3>Phone: {selectedRow.phone}</h3>
-                <h3>Creation Date: {selectedRow.creationDate}</h3>
+                <h2 className="text-xl font-bold mb-4">User Analysis Report</h2>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <div style={{ display: 'flex', marginBottom: '8px' }}>
+                        <span style={{ width: '120px', textAlign: 'left' }}>Name:</span>
+                        <span style={{ textAlign: 'left' }}>{selectedRow.username}</span>
+                    </div>
+                    <div style={{ display: 'flex', marginBottom: '8px' }}>
+                        <span style={{ width: '120px', textAlign: 'left' }}>Email:</span>
+                        <span style={{ textAlign: 'left' }}>{selectedRow.email}</span>
+                    </div>
+                    <div style={{ display: 'flex', marginBottom: '8px' }}>
+                        <span style={{ width: '120px', textAlign: 'left' }}>Phone:</span>
+                        <span style={{ textAlign: 'left' }}>{selectedRow.phone}</span>
+                    </div>
+                    <div style={{ display: 'flex', marginBottom: '8px' }}>
+                        <span style={{ width: '120px', textAlign: 'left' }}>Creation Date:</span>
+                        <span style={{ textAlign: 'left' }}>{selectedRow.creationDate}</span>
+                    </div>
+                </div>
+
                 <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={handlePrint}>
-                    Print PDF
+                    Download PDF
                 </button>
             </div>
         </div>
